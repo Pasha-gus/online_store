77292d1bac59ddcedf070fc86377d523f8cb60f5
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2025-01-17 18:15:21 +0300

    homework 29

--- catalog/views.py
+++ catalog/views.py
@@ -5,13 +5,17 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 from django.http import HttpResponseForbidden
 from django.core.exceptions import PermissionDenied
+from catalog.services import get_products_by_category, get_products_from_cache
 
-from .models import Product
+from .models import Product, Category
 from .forms import ProductForm, ProductModeratorForm
 
 
 class ProductListView(ListView):
     model = Product
+
+    def get_queryset(self):
+        return get_products_from_cache()
 
 
 class ProductDetailView(DetailView):
@@ -39,7 +43,7 @@ class ProductUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_form_class(self):
         user = self.request.user
-        if user == self.object.owner:
+        if user == self.object.owner or user.is_staff:
             return ProductForm
         if user.has_perm("catalog.can_unpublish_product"):
             return ProductModeratorForm
@@ -56,3 +60,14 @@ class ProductDeleteView(LoginRequiredMixin, DeleteView):
         if user == self.object.owner or user.has_perm("catalog.can_unpublish_product"):
             return super().dispatch(request, *args, **kwargs)
         raise PermissionDenied("У вас нет прав для удаления этого продукта.")
+
+
+class CategoryListView(ListView):
+    model = Category
+    template_name = "category_list"
+
+class ProductsByCategoryView(ListView):
+    model = Product
+    def get_queryset(self):
+        category_id = self.kwargs.get('pk')
+        return get_products_by_category(category_id=category_id)
--- config/settings.py
+++ config/settings.py
@@ -143,14 +143,23 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_REDIRECT_URL = '/'
 LOGQUT_REDIRECT_URL = '/'
 
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = "pawel.polezhaev2005@yandex.ru"
-EMAIL_HOST_PASSWORD = "vlyosrwtotglclzh"
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False) == 'True'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', False) == 'True'
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-LOGIN_URL = "users:login"
+LOGIN_URL = 'users:login'
+
+CACHE_ENABLED = True
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': os.getenv('LOCATION')
+        }
+    }
